6e9bb775b8a6d56e2dd8fdd1f1afea32d6af0ce9
faob-dev <farrukh.obaid@gmail.com>
2018-03-29 16:47:44 +0500

    javadoc added for new functional interfaces

--- src/main/java/io/faob/utopian/handlers/ErrorHandler.java
+++ src/main/java/io/faob/utopian/handlers/ErrorHandler.java
@@ -1,5 +1,12 @@
 package io.faob.utopian.handlers;
 
+/**
+ * Functional interface used as lambda expression to handle error.
+ *
+ * @param <E> Exception Type
+ * @author FaoB
+ */
+
 public interface ErrorHandler<E> {
     void handleError(E e);
 }
--- src/main/java/io/faob/utopian/handlers/ResponseHandler.java
+++ src/main/java/io/faob/utopian/handlers/ResponseHandler.java
@@ -1,5 +1,12 @@
 package io.faob.utopian.handlers;
 
+/**
+ * Functional interface used as lambda expression to handle successful response.
+ *
+ * @param <T> Response Type
+ * @author FaoB
+ */
+
 public interface ResponseHandler<T> {
     void handleResponse(T r);
 }
